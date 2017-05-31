a8aefb16839e332bd10c2a9db8ef7d3949117ce1
Grimshaw <GRIMSHSX@legal.regn.net>
2017-05-31 15:13:27 +0100

    Initial commit

---
+++ tests/BotApi.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace BotApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(false);
+        }
+    }
+}
