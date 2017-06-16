e5da9a77cc232b3165d19e169d45b570db4b2d64
Grimshaw <GRIMSHSX@legal.regn.net>
2017-06-16 15:13:59 +0100

    Adding logging

--- src/BotApi/Controllers/VersionController.cs
+++ src/BotApi/Controllers/VersionController.cs
@@ -2,16 +2,26 @@ using System;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BotApi.Controllers
 {
     [Route("api/version")]
     public class VersionController
     {
+        private readonly ILogger<VersionController> _logger;
+
+        public VersionController(ILogger<VersionController> logger)
+        {
+            _logger = logger;
+        }
+
         public Task<string> Get()
         {
             try
             {
+                _logger.LogInformation("Version: GET");
+
                 var versionAttribute = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>();
 
                 if (versionAttribute == null)
