977ffb562a584196633f60eafda1641105ca5eb2
Grimshaw <GRIMSHSX@legal.regn.net>
2017-06-19 15:14:50 +0100

    Adding new logging factory

--- src/BotApi/Startup.cs
+++ src/BotApi/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BotApi.Logging;
 //using Microsoft.Extensions.Logging.Abstractions;
 
 namespace BotApi
@@ -37,7 +37,7 @@ namespace BotApi
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-            //loggerFactory.AddAzureWebAppDiagnostics();
+            //loggerFactory.AddRps();
 
             app.UseMvc();
         }
